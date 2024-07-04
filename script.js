31fabaf5113ab8a9b0a55105b903a3827f696c7c
28flexi06 <aavram26@colby.edu>
2024-07-04 12:10:41 -0400

    rus

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function toggleLanguage() {
+    const enText = document.getElementById('about-en');
+    const ruText = document.getElementById('about-ru');
+
+    if (enText.style.display === 'none') {
+        enText.style.display = 'block';
+        ruText.style.display = 'none';
+    } else {
+        enText.style.display = 'none';
+        ruText.style.display = 'block';
+    }
+}
